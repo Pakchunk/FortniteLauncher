7f141259c117bb5a77020634291c140755edadb7
ozne <68667854+Pakchunk@users.noreply.github.com>
2021-05-09 22:00:36 -0400

    massive code improvement

--- FortniteLauncher/Program.cs
+++ FortniteLauncher/Program.cs
@@ -1,91 +1,76 @@
-using Newtonsoft.Json;
-using ProcessSuspend;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Threading;
+using Newtonsoft.Json;
+using ProcessSuspend;
+using JSON;
 
 namespace FortniteLauncher
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static readonly string LocalAppData = Environment.GetEnvironmentVariable("LocalAppData");
+        static readonly bool WinCheck = Environment.OSVersion.Version < new Version(6, 2);
+        static string Arguments = "TBD";
+
+        static void Main()
         {
             Launch();
         }
 
-
-        public static void Launch()
+        static void Launch()
         {
             Console.Title = "Fortnite Launcher";
-            bool wincheck = Environment.OSVersion.Version < new Version(6, 2);
-            if (wincheck)
+            if (WinCheck)
             {
                 Console.Title = "Error!";
                 Console.WriteLine("A version of Windows other than Windows 10 has been detected, please upgrade as it is completely free!\nhttps://www.microsoft.com/en-us/software-download/windows10 :PeepoGlad:");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
-            else { }
-            string LAD = Environment.GetEnvironmentVariable("LocalAppData");
+            if (File.Exists(LocalAppData + @"\uFortniteLauncher\Settings.json"))
+            { }
+            else { ConfigurateFortnitePath(); }
 
-            if (File.Exists(LAD + @"\uFortniteLauncher\Settings.config"))
-            {
-                File.Delete(LAD + @"\uFortniteLauncher\Settings.config");
-            }
-            else { }
-
-            if (File.Exists(LAD + @"\uFortniteLauncher\Settings.json"))
-            {
-            }
-            else
-            {
-                Directory.CreateDirectory(LAD + @"\uFortniteLauncher");
-                Console.WriteLine("Fortnite Path (Directory that contains Engine and FortniteGame)");
-                Console.Write("> ");
-                string ReadPath = Console.ReadLine().ToLower();
-                using (StreamWriter sw = File.CreateText(LAD + @"\uFortniteLauncher\Settings.json"))
-                {
-                    sw.WriteLine("{");
-                    sw.WriteLine($"  \"Path\": \"{ReadPath}\"");
-                    sw.Write("}");
-                    sw.Close();
-                }
-                string ReplaceThings = File.ReadAllText(LAD + @"\uFortniteLauncher\Settings.json");
-                ReplaceThings = ReplaceThings.Replace(@"\", "\\\\");
-                File.WriteAllText(LAD + @"\uFortniteLauncher\Settings.json", ReplaceThings);
-            }
-            StreamReader sr = new StreamReader(LAD + @"\uFortniteLauncher\Settings.json");
-            string GetPath = sr.ReadToEnd();
-            sr.Close();
-            Settings settings = JsonConvert.DeserializeObject<Settings>(GetPath);
             WebClient webClient = new WebClient();
             var FLToken = webClient.DownloadString("https://api.nitestats.com/v1/epic/builds/fltoken");
+            webClient.Dispose();
             FLTokenJson json = JsonConvert.DeserializeObject<FLTokenJson>(FLToken);
-            
+
             Console.Clear();
-            Console.Write("FL Token: ");
-            Console.Write(json.fltoken);
-            Console.Write("\nFortnite Build: ");
-            Console.Write(json.version);
+            Console.WriteLine("FL Token: " + json.fltoken);
+            Console.WriteLine("Fortnite Build: " + json.version);
+            Console.WriteLine("\nIf you would like to change your Fortnite path, please enter the letter \'c\' instead of your Exchange Code.");
             Console.WriteLine("\n\nExchange Code");
             Console.Write("> ");
-            string exchange = Console.ReadLine().ToLower();
-            if (exchange == "")
+            string exchange = Console.ReadLine();
+            if (exchange == "" || exchange.Contains(" ") || exchange.Contains("~") || exchange.Contains("-") || exchange.Contains("+") || exchange.Contains("`") || exchange.Contains("!") || exchange.Contains("@") || exchange.Contains("#") || exchange.Contains("$") || exchange.Contains("%") || exchange.Contains("^") || exchange.Contains("&") || exchange.Contains("*") || exchange.Contains("(") || exchange.Contains(")") || exchange.Contains("_") || exchange.Contains("=") || exchange.Contains("[") || exchange.Contains("]") || exchange.Contains("{") || exchange.Contains("}") || exchange.Contains("|") || exchange.Contains(@"\") || exchange.Contains(";") || exchange.Contains(":") || exchange.Contains("\'") || exchange.Contains("\"") || exchange.Contains(",") || exchange.Contains(".") || exchange.Contains("<") || exchange.Contains(">") || exchange.Contains("?") || exchange.Contains("/"))
             {
-                Console.WriteLine("Invalid exchange code, exiting...");
-                Thread.Sleep(1200);
+                Console.WriteLine("Invalid Exchange Code!");
+                Thread.Sleep(1300);
                 Environment.Exit(0);
             }
-            else { }
-            string clientargs = $"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -skippatchcheck -NOSSLPINNING -noeac -fromfl=be -fltoken={json.fltoken} -frombe AUTH_TYPE=exchangecode -AUTH_LOGIN=unused -AUTH_PASSWORD={exchange}";
+            if (exchange == "c")
+            {
+                Console.Clear();
+                ConfigurateFortnitePath();
+                Program.Launch();
+                Environment.Exit(0);
+            }
+
+            string GetPath = File.ReadAllText(LocalAppData + @"\uFortniteLauncher\Settings.json");
+            Settings settings = JsonConvert.DeserializeObject<Settings>(GetPath);
+
+            Arguments = $"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -skippatchcheck -NOSSLPINNING -noeac -fromfl=be -fltoken={json.fltoken} -frombe AUTH_TYPE=exchangecode -AUTH_LOGIN=unused -AUTH_PASSWORD={exchange}";
             var client = new Process
             {
                 StartInfo =
                 {
                     FileName = settings.Path + @"\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe",
-                    Arguments = clientargs
+                    Arguments = Arguments,
+                    UseShellExecute = false
                 }
             };
             var eac = new Process
@@ -93,7 +78,8 @@ namespace FortniteLauncher
                 StartInfo =
                 {
                     FileName = settings.Path + @"\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping_EAC.exe",
-                    Arguments = clientargs
+                    Arguments = Arguments,
+                    UseShellExecute = false
                 }
             };
             var launcher = new Process
@@ -101,6 +87,7 @@ namespace FortniteLauncher
                 StartInfo =
                 {
                     FileName = settings.Path + @"\FortniteGame\Binaries\Win64\FortniteLauncher.exe",
+                    UseShellExecute = false
                 }
             };
 
@@ -114,19 +101,7 @@ namespace FortniteLauncher
                 Console.WriteLine("\nInvalid Fortnite Path!\n");
                 Thread.Sleep(1100);
                 Console.Clear();
-                Console.WriteLine("Fortnite Path (Directory that contains Engine and FortniteGame)");
-                Console.Write("> ");
-                string paththing = Console.ReadLine().ToLower();
-                using (StreamWriter sw = File.CreateText(LAD + @"\uFortniteLauncher\Settings.json"))
-                {
-                    sw.WriteLine("{");
-                    sw.WriteLine($"  \"Path\": \"{paththing}\"");
-                    sw.Write("}");
-                    sw.Close();
-                }
-                string ReplaceThings = File.ReadAllText(LAD + @"\uFortniteLauncher\Settings.json");
-                ReplaceThings = ReplaceThings.Replace(@"\", "\\\\");
-                File.WriteAllText(LAD + @"\uFortniteLauncher\Settings.json", ReplaceThings);
+                ConfigurateFortnitePath();
                 Console.Clear();
                 Program.Launch();
                 Environment.Exit(0);
@@ -144,17 +119,27 @@ namespace FortniteLauncher
             Console.WriteLine("VastBlast: NiteStats which is used for grabbing the FL Token");
             Thread.Sleep(2600);
         }
-        
 
-        public class Settings
+        public static void ConfigurateFortnitePath()
         {
-            public string Path { get; set; }
-        }
-        
-        public class FLTokenJson
-        {
-            public string version { get; set; }
-            public string fltoken { get; set; }
+            try
+            { Directory.Delete(LocalAppData + @"\uFortniteLauncher", true); }
+            catch { }
+            Directory.CreateDirectory(LocalAppData + @"\uFortniteLauncher");
+            Console.WriteLine("Fortnite Path (Directory that contains Engine and FortniteGame)");
+            Console.Write("> ");
+            string ReadPath = Console.ReadLine();
+            using (StreamWriter sw = File.CreateText(LocalAppData + @"\uFortniteLauncher\Settings.json"))
+            {
+                sw.WriteLine("{");
+                sw.WriteLine($"  \"Path\": \"{ReadPath}\"");
+                sw.Write("}");
+                sw.Close();
+            }
+            string ReplaceThings = File.ReadAllText(LocalAppData + @"\uFortniteLauncher\Settings.json");
+            ReplaceThings = ReplaceThings.Replace(@"\", @"\\");
+            File.WriteAllText(LocalAppData + @"\uFortniteLauncher\Settings.json", ReplaceThings);
+            return;
         }
     }
 }
