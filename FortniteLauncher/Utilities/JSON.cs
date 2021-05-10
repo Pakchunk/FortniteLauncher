25cc53307709ce76e5b48bb87f12351f9e050084
ozne <68667854+Pakchunk@users.noreply.github.com>
2021-05-09 22:00:33 -0400

    Create JSON.cs

---
+++ FortniteLauncher/Utilities/JSON.cs
@@ -0,0 +1,13 @@
+namespace JSON
+{
+    public class Settings
+    {
+        public string Path { get; set; }
+    }
+
+    public class FLTokenJson
+    {
+        public string version { get; set; }
+        public string fltoken { get; set; }
+    }
+}
